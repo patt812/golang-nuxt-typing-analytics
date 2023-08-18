a7c9aa720f8847cb0ae5a6b7e296f1807e0e9cb7
patt812 <dev.patt812@gmail.com>
2023-08-18 21:55:48 +0900

    set /api prefix

--- backend/api/router.go
+++ backend/api/router.go
@@ -8,14 +8,17 @@ import (
 func Router(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/analyze", func(c *gin.Context) {
-		var version string
-		row := db.Raw("SELECT VERSION()").Row()
-		row.Scan(&version)
+	apiGroup := r.Group("/api")
+	{
+		apiGroup.GET("/analyze", func(c *gin.Context) {
+			var version string
+			row := db.Raw("SELECT VERSION()").Row()
+			row.Scan(&version)
 
-		c.JSON(200, gin.H{
-			"version": version,
+			c.JSON(200, gin.H{
+				"version": version,
+			})
 		})
-	})
+	}
 	return r
 }
