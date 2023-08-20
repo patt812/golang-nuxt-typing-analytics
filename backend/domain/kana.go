d80f17e761525982fb3ce31286f0a8682a554e31
patt812 <dev.patt812@gmail.com>
2023-08-20 17:26:35 +0900

    set default collation to utf8mb4_bin at database level

--- backend/domain/kana.go
+++ backend/domain/kana.go
@@ -2,5 +2,5 @@ package domain
 
 type Kana struct {
 	ID   uint   `gorm:"primaryKey"`
-	Kana string `gorm:"unique,collate:utf8mb4_bin"` // avoid case-insensitive unique constraint
+	Kana string `gorm:"unique"`
 }
