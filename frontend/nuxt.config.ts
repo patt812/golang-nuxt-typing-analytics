09c1e9d9b20d98392509a178682f8b50d6f605b9
patt812 <dev.patt812@gmail.com>
2023-08-23 23:28:58 +0900

    initialize nuxt index & fetch api

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:8080/api'
+    }
+  },
 })
---
+++ frontend/types/api.d.ts
@@ -0,0 +1,5 @@
+export declare global {
+  type AnalyzeResponse = {
+    patterns: string[][]
+  }
+}
